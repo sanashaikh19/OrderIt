46232e9b91f6c699a6ad05a586606052317bd33f
sanashaikh19 <sanarashidshaikh@gmail.com>
2015-12-05 17:14:24 +0530

    Update About.java

--- OrderIt/src/About.java
+++ OrderIt/src/About.java
@@ -69,8 +69,6 @@ public class About implements ActionListener
 		if(e.getSource()==button_close)
 		{
 			MainFrame.dispose();
-			System.out.println("Hi");
-			System.out.println("Hello");
 		}
 		
 	}
