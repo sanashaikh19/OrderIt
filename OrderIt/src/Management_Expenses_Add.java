b69773213dade2ddc569e6b1889d6869af352e3e
sana <sanarashidshaikh@gmail.com>
2015-12-10 16:25:33 +0530

    connected

--- OrderIt/src/Management_Expenses_Add.java
+++ OrderIt/src/Management_Expenses_Add.java
@@ -1,6 +1,9 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 import javax.swing.*;
 
@@ -16,7 +19,7 @@ public class Management_Expenses_Add extends JFrame
 		JLabel_MasterPage_ManagementMenu_Expenses_Add_MainPanel_UserLabel.setPreferredSize(new Dimension(70,30));
 		
 		String User_Combo[]=new String[]{"admin","waiter","Customer","Tarif","Santosh","Shyam"};
-		JComboBox JCombobox_MasterPage_ManagementMenu_Expenses_Add_MainPanel_UserCombo=new JComboBox(User_Combo);
+		final JComboBox JCombobox_MasterPage_ManagementMenu_Expenses_Add_MainPanel_UserCombo=new JComboBox(User_Combo);
 		JCombobox_MasterPage_ManagementMenu_Expenses_Add_MainPanel_UserCombo.setPreferredSize(new Dimension(150,30));
 		//JCombobox_MasterPage_ManagementMenu_Expenses_Add_MainPanel_UserCombo.setBackground(Color.LIGHT_GRAY);
 		
@@ -27,7 +30,8 @@ public class Management_Expenses_Add extends JFrame
 		JLabel_MasterPage_ManagementMenu_Expenses_Add_MainPanel_TypeLabel.setPreferredSize(new Dimension(70,30));
 		
 		String Type_Combo[]=new String[]{"Out","In"};
-		JComboBox JCombobox_MasterPage_ManagementMenu_Expenses_Add_MainPanel_TypeCombo=new JComboBox(Type_Combo);
+		
+		final JComboBox JCombobox_MasterPage_ManagementMenu_Expenses_Add_MainPanel_TypeCombo=new JComboBox(Type_Combo);
 		JCombobox_MasterPage_ManagementMenu_Expenses_Add_MainPanel_TypeCombo.setPreferredSize(new Dimension(150,30));
 		//JCombobox_MasterPage_ManagementMenu_Expenses_Add_MainPanel_UserCombo.setBackground(Color.LIGHT_GRAY);
 		
@@ -37,7 +41,7 @@ public class Management_Expenses_Add extends JFrame
 		JLabel JLabel_MasterPage_ManagementMenu_Expenses_Add_MainPanel_ValueLabel=new JLabel("Value");
 		JLabel_MasterPage_ManagementMenu_Expenses_Add_MainPanel_ValueLabel.setPreferredSize(new Dimension(70,30));
 		
-		JTextField JTextField_MasterPage_ManagementMenu_Expenses_Add_Mainpanel_ValueTextF=new JTextField();
+		final JTextField JTextField_MasterPage_ManagementMenu_Expenses_Add_Mainpanel_ValueTextF=new JTextField();
 		JTextField_MasterPage_ManagementMenu_Expenses_Add_Mainpanel_ValueTextF.setPreferredSize(new Dimension(150,25));
 		
 		JLabel JLabel_EmptyLabel2=new JLabel("");
@@ -46,7 +50,7 @@ public class Management_Expenses_Add extends JFrame
 		JLabel JLabel_MasterPage_ManagementMenu_Expenses_Add_MainPanel_CommentLabel=new JLabel("Comment");
 		JLabel_MasterPage_ManagementMenu_Expenses_Add_MainPanel_CommentLabel.setPreferredSize(new Dimension(70,30));
 		
-		JTextArea JTextArea_MasterPage_ManagementMenu_Expenses_Add_Mainpanel_CommentTextA=new JTextArea();
+		final JTextArea JTextArea_MasterPage_ManagementMenu_Expenses_Add_Mainpanel_CommentTextA=new JTextArea();
 		JTextArea_MasterPage_ManagementMenu_Expenses_Add_Mainpanel_CommentTextA.setPreferredSize(new Dimension(270,110));
 		
 		JLabel JLabel_EmptyLabel3=new JLabel("");
@@ -55,6 +59,38 @@ public class Management_Expenses_Add extends JFrame
 		JButton JButton_MasterPage_ManagementMenu_Expenses_Add_MainPanel_SaveButton=new JButton("Save");
 		JButton_MasterPage_ManagementMenu_Expenses_Add_MainPanel_SaveButton.setPreferredSize(new Dimension(100,45));
 		JButton_MasterPage_ManagementMenu_Expenses_Add_MainPanel_SaveButton.setBackground(Color.LIGHT_GRAY);
+		
+		JButton_MasterPage_ManagementMenu_Expenses_Add_MainPanel_SaveButton.addActionListener(new ActionListener() 
+	    {
+			public void actionPerformed(ActionEvent e) 
+	        {
+			
+					try
+					{
+						Class.forName("com.mysql.jdbc.Driver");  
+						Connection con=DriverManager.getConnection("jdbc:mysql://192.168.1.102:3306/dbc?","root","root");
+						String sql="insert into expenses(User,Type,Value,comments) values(?,?,?,?)";
+						PreparedStatement pstmt = con.prepareStatement(sql);
+
+					    pstmt.setString(1, JCombobox_MasterPage_ManagementMenu_Expenses_Add_MainPanel_UserCombo.getSelectedItem().toString());
+					    pstmt.setString(2, JCombobox_MasterPage_ManagementMenu_Expenses_Add_MainPanel_TypeCombo.getSelectedItem().toString());
+					    pstmt.setString(3, JTextField_MasterPage_ManagementMenu_Expenses_Add_Mainpanel_ValueTextF.getText());
+					    pstmt.setString(4, JTextArea_MasterPage_ManagementMenu_Expenses_Add_Mainpanel_CommentTextA.getText());
+						
+						pstmt.executeUpdate();   
+						JOptionPane.showMessageDialog(null, "Added Successfully","Added",JOptionPane.PLAIN_MESSAGE);
+
+						JTextField_MasterPage_ManagementMenu_Expenses_Add_Mainpanel_ValueTextF.setText("");
+						JTextArea_MasterPage_ManagementMenu_Expenses_Add_Mainpanel_CommentTextA.setText("");
+				}
+
+					catch(Exception ex)
+					{
+						System.out.println(ex);
+						JOptionPane.showMessageDialog(null,"Error In Updation","Error",JOptionPane.ERROR_MESSAGE);
+					}
+				}
+	    });
 		
 		JLabel JLabel_EmptyLabel4=new JLabel("");
 		JLabel_EmptyLabel4.setPreferredSize(new Dimension(60,45));
@@ -87,13 +123,9 @@ public class Management_Expenses_Add extends JFrame
 		Jpanel_MasterPage_ManagementMenu_Expenses_Add_Mainpanel.add(JLabel_EmptyLabel4);
 		Jpanel_MasterPage_ManagementMenu_Expenses_Add_Mainpanel.add(JButton_MasterPage_ManagementMenu_Expenses_Add_MainPanel_CancelButton);
 		
-		
-		
 		frame.add(Jpanel_MasterPage_ManagementMenu_Expenses_Add_Mainpanel);
 		frame.setVisible(true);
 		frame.setSize(400,380);
-	//	frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		frame.setResizable(false);
-		//frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
